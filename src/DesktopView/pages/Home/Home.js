e798d010d6ddd57c82751a7465115e5326117d0c
Corbin Taylor <cptaylor38@yahoo.com>
2020-10-11 01:07:41 -0400

    testing layout methods

--- src/DesktopView/pages/Home/Home.js
+++ src/DesktopView/pages/Home/Home.js
@@ -3,11 +3,20 @@ import './Home.css';
 
 const Home = ({navManager})=> {
     return(
-    <>
-    <div onClick={()=>navManager('about')}>About Me</div>
-    <div onClick={()=>navManager('portfolio')}>Portfolio</div>
-    <div onClick={()=>navManager('faq')}>FAQ</div>
-    </>
+    <div id='cityscape'>
+    <div className='tower tower1'></div>
+    <div className='tower tower2'></div>
+    <div onClick={()=>navManager('about')} className='tower tower3'>About Me</div>
+    <div className='tower tower4'></div>
+    <div className='tower tower5'></div>
+    <div className='tower tower6'></div>
+    <div onClick={()=>navManager('portfolio')} className='tower tower7'>Portfolio</div>
+    <div className='tower tower8'></div>
+    <div className='tower tower9'></div>
+    <div onClick={()=>navManager('faq')} className='tower tower10'>FAQ</div>
+    <div className='tower tower11'></div>
+    <div className='tower tower12'></div>
+    </div>
     )
 }
 
