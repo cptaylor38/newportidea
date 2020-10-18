587d1137350a11d7a3e6aaf00e851b30c9819d90
Corbin Taylor <cptaylor38@yahoo.com>
2020-10-17 22:26:51 -0400

    fixed some breakpoints, added nav text

--- src/DesktopView/pages/Home/Home.js
+++ src/DesktopView/pages/Home/Home.js
@@ -4,9 +4,9 @@ import './Home.css';
 const Home = ({navManager, hoverManager})=> {
     return(
     <div id='cityscape'>
-    <div onClick={()=>navManager('about')} type="button" onMouseEnter={()=>hoverManager('About Me')} onMouseLeave={()=> hoverManager('')} className='tower tower1'>About Me</div>
-    <div onClick={()=>navManager('portfolio')} type="button" onMouseEnter={()=>hoverManager('Portfolio')} onMouseLeave={()=> hoverManager('')} className='tower tower2'>Portfolio</div>
-    <div onClick={()=>navManager('faq')} type="button" onMouseEnter={()=>hoverManager('F.A.Q.')} onMouseLeave={()=> hoverManager('')} className='tower tower3'>FAQ</div>
+    <div onClick={()=>navManager('about')} type="button" onMouseEnter={()=>hoverManager('About Me')} onMouseLeave={()=> hoverManager('')} className='tower tower1'><div id='aboutNavText1'>About Me</div></div>
+    <div onClick={()=>navManager('portfolio')} type="button" onMouseEnter={()=>hoverManager('Portfolio')} onMouseLeave={()=> hoverManager('')} className='tower tower2'><div id='portfolioNavText2'>Portfolio</div></div>
+    <div onClick={()=>navManager('faq')} type="button" onMouseEnter={()=>hoverManager('F.A.Q.')} onMouseLeave={()=> hoverManager('')} className='tower tower3'><div id='faqNavText3'>F.A.Q.</div></div>
     </div>
     )
 }
