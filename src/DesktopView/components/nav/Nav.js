69ca2a3bca8fba3c3c86dcd951b003a2997fb32d
Corbin Taylor <cptaylor38@yahoo.com>
2020-10-19 02:51:34 -0400

    working on page transitions

--- src/DesktopView/components/nav/Nav.js
+++ src/DesktopView/components/nav/Nav.js
@@ -4,8 +4,9 @@ import './Nav.css';
 const Nav = ({navManager, hoverLink}) => {
   return (
   <div id='mobileNav'>
-    <div>Corbin Taylor</div>
+    <div onClick={()=> navManager('home')} id='homeNavItem'>Corbin Taylor</div>
     <div style={{marginRight: '50%'}}>{hoverLink}</div>
+
     {/* <div onClick={()=>navManager('about')}>About</div>
     <div onClick={()=>navManager('home')}>Home</div> */}
   </div>)
--- src/DesktopView/pages/Home/Home.js
+++ src/DesktopView/pages/Home/Home.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Home.css';
 
 const Home = ({navManager, hoverManager})=> {
+    const [style, setStyle] = useState({position: '-200%'})
+
+    useEffect(()=> {
+        let translateX = -200;
+        setInterval(()=> {
+            if(translateX <= 0){
+                setStyle({position: `${translateX}%`})
+                translateX += 20;
+            }
+            else return;
+        }, 35)
+
+        return ()=> {
+            setInterval(()=> {
+                if(translateX >= -200){
+                    setStyle({position: `${translateX}%`})
+                    translateX -= 20;
+                }
+                else return;
+            }, 35)
+        }
+    }, [])
+
+    const animationNavHandler = (navSelection)=> {
+        // let translateX = 90;
+        // setInterval(() => {
+        //     if(height > 0){
+        //         setStyle({height: height + 'vh'})
+        //         height -= 10;
+        //     }
+        //     else return;
+        // }, 35);
+        setTimeout(() => {
+            navManager(navSelection)
+        }, 500);
+    }
+
     return(
     <div id='cityscape'>
-    <div onClick={()=>navManager('about')} type="button" onMouseEnter={()=>hoverManager('About Me')} onMouseLeave={()=> hoverManager('')} className='tower tower1'><div id='aboutNavText1'>About Me</div></div>
-    <div onClick={()=>navManager('portfolio')} type="button" onMouseEnter={()=>hoverManager('Portfolio')} onMouseLeave={()=> hoverManager('')} className='tower tower2'><div id='portfolioNavText2'>Portfolio</div></div>
-    <div onClick={()=>navManager('faq')} type="button" onMouseEnter={()=>hoverManager('F.A.Q.')} onMouseLeave={()=> hoverManager('')} className='tower tower3'><div id='faqNavText3'>F.A.Q.</div></div>
+    <div onClick={()=> animationNavHandler('about')} type="button" onMouseEnter={()=>hoverManager('About Me')} onMouseLeave={()=> hoverManager('')} style={{transform: `translateX(${style.position})`}} className='tower tower1'><div id='aboutNavText1'>About Me</div></div>
+    <div onClick={()=>animationNavHandler('portfolio')} type="button" onMouseEnter={()=>hoverManager('Portfolio')} onMouseLeave={()=> hoverManager('')} style={{transform: `translateX(${style.position})`}} className='tower tower2'><div id='portfolioNavText2'>Portfolio</div></div>
+    <div onClick={()=>animationNavHandler('faq')} type="button" onMouseEnter={()=>hoverManager('F.A.Q.')} onMouseLeave={()=> hoverManager('')}style={{transform: `translateX(${style.position})`}} className='tower tower3'><div id='faqNavText3'>F.A.Q.</div></div>
     </div>
     )
 }
