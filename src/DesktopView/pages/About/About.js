e6b7f6fe4e6c047a0a71eaba6ef22f00204669f4
Corbin Taylor <cptaylor38@yahoo.com>
2020-10-25 15:25:10 -0400

    starting on positioning event listeners

--- src/DesktopView/pages/About/About.js
+++ src/DesktopView/pages/About/About.js
@@ -1,29 +1,45 @@
 import React, {useState} from 'react';
 import './About.css';
 const bioImage = require('../../../assets/images/selfieLinkedIn.png')
-
+//p1 270, p2 180, p3 0, p4 90
 const About = () => {
     const [puzzleOrientation, setPuzzleOrientation] = useState({
-        p1: 270,
-        p2: 180,
-        p3: 0,
-        p4: 90
+        p1: {
+            rotation: 270,
+            translateX: 20,
+            translateY: 20 
+        },
+        p2: {
+            rotation: 180,
+            translateX: 40,
+            translateY: 80
+        },
+        p3: {
+            rotation: 0,
+            translateX: 40,
+            translateY: 60
+        },
+        p4: {
+            rotation: 90,
+            translateX: 20,
+            translateY: 80
+        }
     })
 
     const rotatePiece = (piece, currentOrientation) => {
         let orientation = currentOrientation + 90;
         switch(piece){
             case 'p1':
-                setPuzzleOrientation({...puzzleOrientation ,p1: orientation})
+                setPuzzleOrientation({...puzzleOrientation, p1: {...puzzleOrientation.p1, rotation: orientation}})
                 break;
             case 'p2':
-                setPuzzleOrientation({...puzzleOrientation ,p2: orientation} )
+                setPuzzleOrientation({...puzzleOrientation, p2: {...puzzleOrientation.p2, rotation: orientation}} )
                 break;
             case 'p3':
-                setPuzzleOrientation({...puzzleOrientation ,p3: orientation})
+                setPuzzleOrientation({...puzzleOrientation, p3: {...puzzleOrientation.p3, rotation: orientation}})
                 break;
             case 'p4':
-                setPuzzleOrientation({...puzzleOrientation ,p4: orientation})
+                setPuzzleOrientation({...puzzleOrientation, p4: {...puzzleOrientation.p4, rotation: orientation}})
                 break;
             default:
                 return;
@@ -35,14 +51,12 @@ const About = () => {
     <div id='aboutCont'>
     {/* <img src={bioImage} id='selfie'/> */}
     <div id='pContainer'>
-    <div id='puzzleP1' onClick={()=> rotatePiece('p1', puzzleOrientation.p1)} style={{transform: `rotate(${puzzleOrientation.p1}deg)`}}>a</div>
-    <div id='puzzleP2' onClick={()=> rotatePiece('p2', puzzleOrientation.p2)} style={{transform: `rotate(${puzzleOrientation.p2}deg)`}}>a</div>
-    <div id='puzzleP3' onClick={()=> rotatePiece('p3', puzzleOrientation.p3)}style={{transform: `rotate(${puzzleOrientation.p3}deg)`}}>a</div>
-    <div id='puzzleP4' onClick={()=> rotatePiece('p4', puzzleOrientation.p4)}style={{transform: `rotate(${puzzleOrientation.p4}deg)`}}>a</div>
+    <div id='puzzleP1' className='puzzlePiece' onClick={()=> rotatePiece('p1', puzzleOrientation.p1.rotation)} style={{transform: `rotate(${puzzleOrientation.p1.rotation}deg) translate(${puzzleOrientation.p1.translateX}px, ${puzzleOrientation.p1.translateY}px)`}}>a</div>
+    <div id='puzzleP2' className='puzzlePiece' onClick={()=> rotatePiece('p2', puzzleOrientation.p2.rotation)} style={{transform: `rotate(${puzzleOrientation.p2.rotation}deg) translate(${puzzleOrientation.p2.translateX}px, ${puzzleOrientation.p2.translateY}px)`}}>a</div>
+    <div id='puzzleP3' className='puzzlePiece' onClick={()=> rotatePiece('p3', puzzleOrientation.p3.rotation)} style={{transform: `rotate(${puzzleOrientation.p3.rotation}deg) translate(${puzzleOrientation.p3.translateX}px, ${puzzleOrientation.p3.translateY}px)`}}>a</div>
+    <div id='puzzleP4' className='puzzlePiece' onClick={()=> rotatePiece('p4', puzzleOrientation.p4.rotation)} style={{transform: `rotate(${puzzleOrientation.p4.rotation}deg) translate(${puzzleOrientation.p4.translateX}px, ${puzzleOrientation.p4.translateY}px)`}}>a</div>
     </div>
-    <div id='pPlacementCont'>
-
-    </div>
+    <div id='pPlacementCont'></div>
     </div>
     )
 }
